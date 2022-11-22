0bada3fff905f03be5fe65a58d1a806ca79555f9
Uriel Federez <uafederez@gmail.com>
2022-11-22 22:57:46 +0800

    Modified navbar styles for partial width of the screen

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,20 +3,22 @@ import "../styles/nav-style.css";
 
 const Navigation = () => (
     <div className="nav-container">
-        <h1 className="logo-text">Interactive ML</h1>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/">About</Link>
-                </li>
-                <li>
-                    <Link to="/">AI Models</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <h1 className="logo-text">Interactive ML</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/">AI Models</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 );
 
