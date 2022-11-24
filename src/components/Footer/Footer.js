c925d5982cedc775d8e623fcfdaaf9a723e4c408
Uriel Federez <uafederez@gmail.com>
2022-11-24 19:42:56 +0800

    Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,38 @@
+import "./Footer-styles.css";
+
+const Footer = () => (
+    <footer>
+        <div className="container">
+            <div>
+                <p>Built by</p>
+                <p>Uriel Federez</p>
+            </div>
+            <div>
+                <p>Built with</p>
+                <p>ReactJS</p>
+                <p>Flask RESTful</p>
+                <p>Python</p>
+                <p>Numpy</p>
+                <p>Custom CSS</p>
+            </div>
+            <div>
+                <p>Contact</p>
+                <p>
+                    <a href="mailto:urielfederez@gmail.com">
+                        urielfederez@gmail.com
+                    </a>
+                </p>
+                <p>
+                    <a href="https://www.linkedin.com/in/uriel-federez-26598723b/">
+                        LinkedIn
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/UAFederez">Github</a>
+                </p>
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer;
